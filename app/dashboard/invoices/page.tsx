6d6a51184e400772a28f260e7e8069701d497acd
Markus Vaananen <markus.vaananen@gmail.com>
2023-12-21 10:52:07 +0200

    Metadata to pages

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -11,7 +11,7 @@ import { Metadata } from 'next';
 export const metadata: Metadata = {
   title: 'Invoices',
 };
- 
+
 export default async function Page({
         searchParams,
     }: {
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,10 +5,13 @@ import { Metadata } from 'next';
 export const metadata: Metadata = {
   title: {
     template: '%s | Acme Dashboard',
-    default: 'Acme Dashboard',
+    default: 'Acme Dashboard'
   },
   description: 'The official Next.js Learn Dashboard built with App Router.',
   metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+  keywords: ['Next.js', 'React', 'JavaScript'],
+  
+
 };
 
 export default function RootLayout({
